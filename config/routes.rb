01896edd16e7ce0695539fd0f1524c5f3dab81d5
Ely Hess <Ely.hess@me.com>
2021-02-20 11:07:06 -0700

    remove portfolio_coins resource
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'portfolios#index'
-  resources :portfolios do
-    resources :coins, controller: 'portfolio_coins', only: [:new, :create]
-  end
+  resources :portfolios
 end
