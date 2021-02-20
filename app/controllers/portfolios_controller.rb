7501ca628df809ed86097492042b0c1108573675
Ely Hess <Ely.hess@me.com>
2021-02-20 11:06:47 -0700

    Add create/destroy actions
    Co-authored-by: Sam Yeo <10618376+SK-Sam@users.noreply.github.com>

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,6 +2,7 @@ class PortfoliosController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @portfolios = current_user.portfolios
   end
 
   def new
@@ -9,7 +10,32 @@ class PortfoliosController < ApplicationController
   end
 
   def create
-    @portfolio = current_user.portfolios.create(params[:name])
-    redirect_to new_portfolio_coin_path(@portfolio)
+    @portfolio = current_user.portfolios.new(name: params[:portfolio][:name])
+    if @portfolio.save && @portfolio.add_portfolio_coins(portfolio_params)
+      redirect_to portfolios_path, notice: "Successfully created portfolio."
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    Portfolio.destroy(params[:id])
+    redirect_to portfolios_path, notice: "Successfully deleted portfolio."
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(
+                                      :bitcoin,
+                                      :ethereum,
+                                      :monero,
+                                      :uniswap,
+                                      :garlicoin,
+                                      :decentraland,
+                                      :stellar,
+                                      :ripple,
+                                      :litecoin,
+                                      :chainlink)
   end
 end
