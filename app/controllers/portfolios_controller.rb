dcb7b3e5dab58de9b0a8a0e5ac7235476cdb8caf
Sam Yeo <yeosamuel95@gmail.com>
2021-02-20 12:50:05 -0700

    Unfinished Total functionality
    Co-authored-by: Ely Hess <46981974+elyhess@users.noreply.github.com>

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,6 +3,34 @@ class PortfoliosController < ApplicationController
 
   def index
     @portfolios = current_user.portfolios
+    # Get all coin names from all portfolios and only get name once
+    @portfolio_coin_names = @portfolios.map do |portfolio|
+      portfolio.portfolio_coins.map do |portfolio_coin|
+        portfolio_coin.coin_name
+      end
+    end.flatten.uniq
+
+    # @portfolio_coin_names = ["bitcoin"]
+    query_string = @portfolio_coin_names.join(",")
+    # query_string "bitcoin"
+    response = Faraday.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=#{query_string}")
+    data = JSON.parse(response.body, symbolize_names: true)
+    @currencies = data.map do |coin_data|
+      Currency.new(coin_data)
+    end
+    
+    coin_value_dictionary = Hash.new()
+    @currencies.each do |crypto|
+      coin_value_dictionary[crypto.name] = crypto.price
+    end
+
+    #Poro sample
+    # coin_values = {
+    #  bitcoin: 5042,
+    #  ethereum: 2000,
+    #  dogecoin: 0.004
+    # }
+
   end
 
   def new
