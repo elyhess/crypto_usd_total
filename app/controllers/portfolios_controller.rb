56ca3210f47e97410e88ec62b775dfb36fa95989
Ely Hess <Ely.hess@me.com>
2021-02-20 20:19:22 -0700

    Refactor index action into facade

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -3,41 +3,13 @@ class PortfoliosController < ApplicationController
 
   def index
     @portfolios = current_user.portfolios
-    # Get all coin names from all portfolios and only get name once
-    @portfolio_coin_names = @portfolios.map do |portfolio|
-      portfolio.portfolio_coins.map do |portfolio_coin|
-        portfolio_coin.coin_name
-      end
-    end.flatten.uniq
+    @currencies = CurrencyFacade.get_currencies(@portfolios.get_coins)
+    @grand_total = PortfoliosFacade.grand_total(@portfolios, @currencies)
+  end
 
-    # @portfolio_coin_names = ["bitcoin"]
-    query_string = @portfolio_coin_names.join(",")
-    # query_string "bitcoin"
-    response = Faraday.get("https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=#{query_string}")
-    data = JSON.parse(response.body, symbolize_names: true)
-    @currencies = data.map do |coin_data|
-      Currency.new(coin_data)
-    end
-    
-    coin_value_dictionary = Hash.new
-    @currencies.each do |crypto|
-      coin_value_dictionary[crypto.name] = crypto.price
-    end
-
-    @portfolios.each do |portfolio|
-      individual_portfolio_total_value = portfolio.coins.sum do |coin|
-        coin_value_dictionary[coin.name] * PortfolioCoin.find_by(coin_id: coin.id).quantity
-      end
-      portfolio.update(total: individual_portfolio_total_value)
-    end
-
-    #Poro sample
-    # coin_values = {
-    #  bitcoin: 5042,
-    #  ethereum: 2000,
-    #  dogecoin: 0.004
-    # }
-
+  def show
+    @portfolio = Portfolio.find(params[:id])
+    @currencies = CurrencyFacade.get_currencies(@portfolio.get_coins)
   end
 
   def new
@@ -61,8 +33,7 @@ class PortfoliosController < ApplicationController
   private
 
   def portfolio_params
-    params.require(:portfolio).permit(
-                                      :bitcoin,
+    params.require(:portfolio).permit(:bitcoin,
                                       :ethereum,
                                       :monero,
                                       :uniswap,
@@ -73,4 +44,5 @@ class PortfoliosController < ApplicationController
                                       :litecoin,
                                       :chainlink)
   end
+
 end
